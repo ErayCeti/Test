b1854eec356966bdb980a9162f2e8a8e7d0a6c51
Doddger34 <cetin.eray@hotmail.com>
2020-06-16 21:34:00 +0300

    fix

--- routes/home.js
+++ routes/home.js
@@ -1,31 +1,6 @@
 const express = require('express');
 const router = express.Router();
-//const mysql = require('mysql');
 
-// const con = mysql.createConnection({
-//   host:"localhost",
-//   user:"root",
-//   password:"",
-//   database:"Notcu"
-// })
-//
-// con.connect(err =>{
-//   if(err) console.log(err);
-// })
-//
-// router.get('/home',(req, res, next)=>{
-//   const sorgu = "SELECT * FROM notlar ORDER BY RAND() LIMIT 4"
-//   con.query(sorgu, (err,result)=>{
-//     if(err){
-//       return res.send(err)
-//     }
-//     else{
-//       return res.json({
-//         data:result
-//       })
-//     }
-//   })
-// })
 router.get('/home', (req, res, next) => {
     res.status(200).json({
         Name:'Eray',
